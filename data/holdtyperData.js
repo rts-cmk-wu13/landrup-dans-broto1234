be4de704d7c510028047da408eaea15af9e092a5
broto1234 <satyo97du@gmail.com>
2026-02-18 14:22:53 +0100

    Updated Home page

---
+++ data/holdtyperData.js
@@ -0,0 +1,26 @@
+export const holdtyperData = [
+  {
+    id: 1,
+    name: "Børnehold",
+    description: "På børneholdene leger vi os ind i dansens verden gennem musik, bevægelse og fantasi. Undervisningen styrker motorik, rytme og kropsbevidsthed i trygge rammer. Fokus er på danseglæde, fællesskab og aktiv bevægelse, hvor alle kan være med.",
+    image: "/boernedans.jpg"
+  },
+  {
+    id: 2,
+    name: "Selskabs- og seniordans",
+    description: "Selskabs- og seniordans kombinerer hyggeligt samvær med skånsom motion. Vi danser klassiske pardanse i et tempo, hvor alle kan følge med. Undervisningen styrker balance, koordination og kondition, samtidig med at fællesskabet og danseglæden er i centrum.",
+    image: "/seniordans.jpg"
+  },
+  {
+    id: 3,
+    name: "Moderne dans og ballet",
+    description: "Moderne dans og ballet forener teknik, kropskontrol og musikalsk udtryk. Træningen forbedrer styrke, smidighed og holdning gennem varierede øvelser. Undervisningen foregår i en positiv atmosfære, hvor bevægelsesglæde og koncentration skaber både fordybelse og effektiv motion.",
+    image: "/modernedans.JPG"
+  },
+  {
+    id: 4,
+    name: "Streetdance og hiphop",
+    description: "Streetdance og hiphop er energifyldt træning med fokus på rytme, attitude og fællesskab. Vi arbejder med grooves, koreografier og grundtrin, der styrker kondition og koordination. Stemningen er uformel og motiverende, så motion og danseglæde går hånd i hånd.",
+    image: "/streethiphop.jpg"  
+  }
+];
